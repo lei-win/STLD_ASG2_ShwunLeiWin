36bfce2b2b1657cd527577c20c759ec28c76ba69
lei-win <s10272765@connect.np.edu.sg>
2026-07-06 01:35:16 +0800

    metal scrap collectible

--- Assets/Scriptz/MetalScrap.cs
+++ Assets/Scriptz/MetalScrap.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 
-// Inherits from Collectible!
-public class MetalScrap : Collectible 
+public class MetalScrap : Collectible
 {
     public override void OnCollect()
     {
-        // Add 1 to the scrap count in our Game Manager
-        GameManager.instance.metalScraps += 1;
+        // 1. Add 1 to our metal scraps count
+        GameManager.instance.metalScraps++;
+        Debug.Log("Collected a metal scrap! Total scraps: " + GameManager.instance.metalScraps);
         
-        Debug.Log("Got a metal scrap! Total Scraps: " + GameManager.instance.metalScraps);
-        
-        // Destroy the object
+        // 2. Check if this was the final piece
+        if (GameManager.instance.metalScraps >= 3)
+        {
+            WinGame();
+        }
+
+        // 3. Play destruction/collect effects from the base class
         base.OnCollect(); 
+    }
+
+    private void WinGame()
+    {
+        Debug.Log("YOU WIN! Collected all 3 metal scraps. Game Over!");
+        
+        // Freezes the game simulation completely
+        Time.timeScale = 0f; 
+
+        // TODO: This is where we will activate your "Victory/Credits" UI screen next!
     }
 }
